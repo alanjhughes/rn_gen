3106cf3c6fec32c21b6cfdc6b1a7be4ceb1ed835
Alan Hughes <hughesno1@gmail.com>
2022-05-02 21:36:28 +0100

    wip: expo support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+use std::fmt::format;
 use std::fs::{self, ReadDir};
+use std::io::{self, Write};
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 use clap::{Parser, Subcommand};
 
@@ -19,15 +22,23 @@ struct Cli {
 enum Commands {
     #[clap(arg_required_else_help = true)]
     #[clap(about = "The name of your new project")]
-    New { name: String },
+    New {
+        name: String,
+        #[clap(long = "expo")]
+        expo: bool,
+    },
 }
 
 pub fn run() {
     let args = Cli::parse();
 
     match args.cmd {
-        Commands::New { name } => {
-            generate_project(name);
+        Commands::New { name, expo } => {
+            if expo {
+                generate_expo(name);
+            } else {
+                generate_project(name);
+            }
         }
     }
 }
@@ -39,25 +50,33 @@ fn generate_project(name: String) {
     create_dir(&target.to_path_buf(), &path.to_path_buf());
 }
 
+fn generate_expo(name: String) {
+    let expo = Command::new("npx")
+        .arg("expo-cli")
+        .arg("init")
+        .arg(name)
+        .arg("--template")
+        .arg("blank")
+        .arg("--non-interactive")
+        .output()
+        .expect("Expo not available");
+
+    io::stdout().write_all(&expo.stdout).unwrap();
+    io::stderr().write_all(&expo.stderr).unwrap();
+}
+
 fn copy_file(from: PathBuf, to: PathBuf) {
-    match fs::copy(from, to) {
-        Ok(res) => {
-            println!("{} has been copied!", res)
-        }
-        Err(err) => println!("{} failed to copy", err.kind()),
-    }
+    fs::copy(from, to).expect("Failed to copy file");
 }
 
 fn create_dir(new_dir: &Path, parent: &Path) {
-    match fs::create_dir(&new_dir) {
-        Err(err) => println!("Failed to create directory: {}", err.kind()),
-        Ok(()) => println!("Directory created"),
-    }
+    fs::create_dir(&new_dir).expect("Failed to create directory");
+
     let entries = get_dir_entries(parent);
 
     for entry in entries {
         if let Ok(entry) = entry {
-            let metadata = entry.metadata().unwrap();
+            let metadata = entry.metadata().expect("Failed to read file metadata");
             if metadata.is_dir() {
                 let dir_path = new_dir.join(entry.file_name());
                 create_dir(&dir_path.to_path_buf(), &entry.path());
@@ -69,8 +88,5 @@ fn create_dir(new_dir: &Path, parent: &Path) {
 }
 
 fn get_dir_entries(path: &Path) -> ReadDir {
-    match fs::read_dir(path) {
-        Err(err) => panic!("Failed to read {}", err.kind()),
-        Ok(entries) => entries,
-    }
+    fs::read_dir(path).expect("Failed to read directory")
 }
